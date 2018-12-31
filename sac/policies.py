33bf38184f43641f686b8cd11db0bfc1a02a1a21
Ethan Brooks <ethanabrooks@gmail.com>
2018-12-31 10:55:11 -0500

    reformat

--- sac/policies.py
+++ sac/policies.py
@@ -80,8 +80,8 @@ class CategoricalPolicy(AbstractAgent):
     @staticmethod
     def policy_parameters_to_log_prob(action, parameters):
         logits = parameters
-        return tf.distributions.Categorical(logits=logits).log_prob(tf.argmax(action,
-                                                                              axis=1))
+        return tf.distributions.Categorical(logits=logits).log_prob(
+            tf.argmax(action, axis=1))
 
     @staticmethod
     def policy_parameters_to_sample(parameters):
